ebcb5a38fab5f04d68c75fedc67f60720fa7f2c5
wodahs18 <flaviofilho180399@gmail.com>
2019-09-19 22:10:03 -0300

    First Commit

---
+++ database/migrations/2019_09_20_005123_cliente.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Cliente extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->timestamps();
+            $table->softdeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('styles')
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+@stop
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <div class="row">
+            <i class="material-icons mr-1" style="font-size:50px;">home</i>
+            <h1>Home</h1>
+        </div>
+    </div>
+    <span>Teste</span>
+</div>
+@stop
